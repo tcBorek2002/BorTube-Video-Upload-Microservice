6618b6f5b9ddd4f80aa8a68d44b5c0457c7fdbff
Borek Bandell <borekbandell@hotmail.com>
2024-04-24 21:52:47 +0200

    Implemented basic SAS creation

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import { VideoService } from './services/implementations/VideoService';
 import { PrismaVideoRepository } from './repositories/implementations/PrismaVideoRepository';
 import Connection, { ConnectionOptions } from 'rabbitmq-client';
-import { VideoRouterRabbit } from './routes/VideoRouterRabbit';
+import { UploadRouterRabbit } from './routes/UploadRouterRabbit';
+import { AzureUploadService } from './services/implementations/AzureUploadService';
 
 //For env File 
 dotenv.config();
@@ -10,7 +11,7 @@ dotenv.config();
 // RabbitMQ connection
 const userName = "NodeUser";
 const password = process.env.RABBITMQ_PASSWORD;
-const options: ConnectionOptions = { username: userName, password: password, connectionName: 'Video Microservice', hostname: "217.105.22.226" };
+const options: ConnectionOptions = { username: userName, password: password, connectionName: 'Video Uploading Microservice', hostname: "217.105.22.226" };
 const rabbit = new Connection(options);
 
 rabbit.on('error', (err) => {
@@ -19,5 +20,5 @@ rabbit.on('error', (err) => {
 rabbit.on('connection', () => {
   console.log('Connection successfully (re)established')
 })
-const videoRouterRabbit = new VideoRouterRabbit(rabbit, new VideoService(new PrismaVideoRepository()));
-videoRouterRabbit.start();
+const uploadRouterRabbit = new UploadRouterRabbit(rabbit, new AzureUploadService());
+uploadRouterRabbit.start();
---
+++ routes/UploadRouterRabbit.ts
@@ -0,0 +1,66 @@
+import { Connection, Envelope } from 'rabbitmq-client';
+import { IVideoService } from '../services/IVideoService';
+import { Video } from '@prisma/client';
+import { ResponseDto } from '../dtos/ResponseDto';
+import { IUploadService } from '../services/IUploadService';
+import { ErrorDto } from '../dtos/ErrorDto';
+
+
+async function rabbitReply(reply: (body: any, envelope?: Envelope | undefined) => Promise<void>, response: ResponseDto<any>): Promise<void> {
+    await reply(response);
+}
+
+export class UploadRouterRabbit {
+    private rabbit: Connection;
+    private uploadService: IUploadService;
+
+    constructor(rabbit: Connection, uploadService: IUploadService) {
+        this.rabbit = rabbit;
+        this.uploadService = uploadService;
+    }
+
+    public start(): void {
+        const getUploadUrl = this.rabbit.createConsumer(
+            {
+                queue: 'get-upload-url',
+            },
+            async (req, reply) => {
+                console.log('Get upload url request:', req.body);
+                if (req.body == null) {
+                    return await rabbitReply(reply, new ResponseDto(false, new ErrorDto(400, 'InvalidInputError', 'VideoID and Filename are required.')));
+                }
+                const { videoId, fileName } = req.body;
+                if (!videoId || !fileName) {
+                    return await rabbitReply(reply, new ResponseDto(false, new ErrorDto(400, 'InvalidInputError', 'VideoID and Filename are required.')));
+                }
+                try {
+                    const url = this.uploadService.getUploadUrl(videoId, fileName);
+                    rabbitReply(reply, new ResponseDto<{ url: string }>(true, { url }));
+                }
+                catch (error) {
+                    console.error(error);
+                    rabbitReply(reply, new ResponseDto(false, new ErrorDto(500, 'InternalServerError', 'An error occurred while generating the upload URL.' + error)));
+                }
+
+            }
+        );
+
+        const checkUploadState = this.rabbit.createConsumer(
+            {
+                queue: 'check-upload-state',
+            },
+            async (req, reply) => {
+                console.log('Get all visible videos request:', req.body);
+                rabbitReply(reply, new ResponseDto<Video[]>(false, new ErrorDto(501, 'NotImplementedError', 'Not implemented')));
+            }
+        );
+
+        process.on('SIGINT', async () => {
+            await Promise.all([
+                getUploadUrl.close(),
+                checkUploadState.close(),
+            ]);
+            await this.rabbit.close();
+        });
+    }
+}
